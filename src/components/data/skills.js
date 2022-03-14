38ede0963509147f41dac6f1eab891c4b1326c4c
HaxXeD <eziovain@gmail.com>
2022-03-14 12:02:23 +0545

    updates

--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -13,10 +13,6 @@ export const SkillsData = [
             {
                 name:"Photoshop" ,
                 icon:<i class="devicon-photoshop-line"></i>
-            },
-            {
-                name:"Blender" ,
-                icon: <img src="https://img.icons8.com/windows/512/000000/blender-3d.png" width={80}/>
             }
         ]
     },
@@ -34,10 +30,6 @@ export const SkillsData = [
             {
                 name:"Photoshop" ,
                 icon:<i class="devicon-photoshop-line"></i>
-            },
-            {
-                name:"Blender" ,
-                icon: <img src="https://img.icons8.com/windows/512/000000/blender-3d.png" width={80}/>
             }
         ]
     }
